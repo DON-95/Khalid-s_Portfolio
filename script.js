954bcf3b7d16292f67ae955c9b97df8e993d45f2
Khalid Shaikh <khalidthedev95@gmail.com>
2024-10-18 09:56:01 +0530

    Added dark mode conversions

--- script.js
+++ script.js
@@ -1,7 +1,20 @@
 const menuIcon = document.querySelector('#menu-btn');
 const navLinks = document.querySelector('.nav-links');
+const darkModeToggle = document.querySelector('#dark-mode-toggle');
 
 menuIcon.onclick = () => {
     navLinks.classList.toggle('active');
 }
 
+darkModeToggle.onchange = () => {
+    document.body.classList.toggle('dark-mode');
+    const elementsToToggle = [
+        document.querySelector('.header'),
+        ...document.querySelectorAll('a'),
+        ...document.querySelectorAll('.grid-card'),
+        ...document.querySelectorAll('.projects-card'),
+        document.querySelector('footer')
+    ];
+    
+    elementsToToggle.forEach(element => element.classList.toggle('dark-mode'));
+};
